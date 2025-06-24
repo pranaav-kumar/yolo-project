a1f5778adaee9a94e77dcd14643aac470226273c
pranaav kumar <pranaavkumarj@gmail.com>
2025-06-24 09:59:14 +0530

    first page done

--- src/Project.jsx
+++ src/Project.jsx
@@ -1,15 +1,42 @@
-function Project(){
-    return(
-        <div>
-        <h1>Create a process:</h1>
-        <h3>process name:</h3>
-        <input></input>
-        <h3>process description:</h3>
-        <input></input>
-        <br/><br/>
-        <button>SUBMIT</button>
+import { useState } from "react";
+import Steps from './Steps'
 
+function Project() {
+    const [projectName, setProjectName] = useState("");
+    const [inputValue, setInputValue] = useState(""); 
+    const [projectDesc, setProjectDesc] = useState("");
+    const [descValue, setDescValue] = useState(""); 
+
+    const btnClick = () => {
+        setProjectName(inputValue); 
+    };
+
+    if (projectName === "") {
+        return (
+            <>
+                <h3>Enter project name:</h3>
+                <input
+                    value={inputValue}
+                    onChange={(e) => setInputValue(e.target.value)}
+                />
+
+                <h3>Enter project description:</h3>
+                <input
+                    value={descValue}
+                    onChange={(e) => setDescValue(e.target.value)}
+                />
+                <button onClick={btnClick}>Submit</button>
+            </>
+        );
+    }
+
+    return (
+        <div>
+            <h1>{projectName}</h1>
+            <h3>{descValue}</h3>
+            <Steps/>
         </div>
-    )
+    );
 }
-export default Project
+
+export default Project;
---
+++ src/Steps.jsx
@@ -0,0 +1,9 @@
+function Steps(){
+    return(
+        <div>
+            <h3>Add steps:</h3>
+        </div>
+    );
+}
+
+export default Steps
