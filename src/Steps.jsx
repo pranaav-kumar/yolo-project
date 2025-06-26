56c0746f2aa1482a898d09bf51dcdd6051eaab29
Sreelayaa-S <231501159@rajalakshmi.edu.in>
2025-06-26 13:22:27 +0530

    changes

--- src/Steps.jsx
+++ src/Steps.jsx
@@ -21,12 +21,11 @@ function Steps() {
 
 
     return (
-        <div className="conatiner mt-4">
+        <div className="container mt-4">
             <h3 className="mb-3"> Add steps:</h3>
             <button className="btn btn-outline-success me-2" onClick={addSteps}>+</button>
             {control && <CreateSteps onSubmit={handleSubmit} />}
-            
-            <br> </br>
+            <br></br>
 
             <button className="btn btn-primary" onClick={()=>navigate('/steps',{ state: { stepArr: stepVal } })}>STEPS</button>
 
